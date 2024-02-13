681ffdcfc13e8c94d8ff4dd074e2032b8250cee2
pofagyalu <pofagyalu@gmail.com>
2024-02-13 17:14:01 +0100

    removed checkBody middleware

--- server/src/tour/tour.controller.js
+++ server/src/tour/tour.controller.js
@@ -1,15 +1,5 @@
 // import Tour from './tour.model.js';
 
-export const checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
-
 export const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
