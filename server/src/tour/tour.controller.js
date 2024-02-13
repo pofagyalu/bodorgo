f10262620f78140178c22614fd052eb0573573cc
pofagyalu <pofagyalu@gmail.com>
2024-02-13 17:35:42 +0100

    createTour modified wo work with db

--- server/src/tour/tour.controller.js
+++ server/src/tour/tour.controller.js
@@ -1,4 +1,4 @@
-// import Tour from './tour.model.js';
+import Tour from './tour.model.js';
 
 export const getAllTours = (req, res) => {
   res.status(200).json({
@@ -8,7 +8,23 @@ export const getAllTours = (req, res) => {
 
 export const getTour = (req, res) => {};
 
-export const createTour = (req, res) => {};
+export const createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
 
 export const updateTour = (req, res) => {
   res.status(200).json({
