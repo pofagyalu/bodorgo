92f70719cdbee5d8c29af79d78f172eb8216f283
pofagyalu <pofagyalu@gmail.com>
2024-02-13 18:46:53 +0100

    updated tour model

--- server/src/tour/tour.model.js
+++ server/src/tour/tour.model.js
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 
 const tourSchema = new mongoose.Schema(
   {
+    duration: {
+      type: Number,
+      required: true,
+    },
     name: {
       type: String,
       required: [true, 'A tour must have a name'],
