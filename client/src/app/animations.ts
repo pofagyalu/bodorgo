04ee751ab52175c87c536b3a05ce0f3f17fbfe77
pofagyalu <pofagyalu@gmail.com>
2024-03-04 11:17:26 +0100

    added page animation

---
+++ client/src/app/animations.ts
@@ -0,0 +1,41 @@
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  animateChild,
+  group,
+} from '@angular/animations';
+
+export const slideInAnimation = trigger('routeAnimations', [
+  transition('* <=> *', [
+    style({ position: 'relative' }),
+    query(
+      ':enter, :leave',
+      [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+        }),
+      ],
+      { optional: true }
+    ),
+    query(':enter', [style({ left: '-100%' })], { optional: true }),
+    query(':leave', animateChild(), { optional: true }),
+    group([
+      query(
+        ':leave',
+        [animate('0.3s ease-out', style({ left: '100%', opacity: 0 }))],
+        { optional: true }
+      ),
+      query(':enter', [animate('0.6s ease-out', style({ left: '0%' }))], {
+        optional: true,
+      }),
+      query('@*', animateChild(), { optional: true }),
+    ]),
+  ]),
+]);
