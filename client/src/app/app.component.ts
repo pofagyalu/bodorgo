1653f49a237a1f63c8599da66c4a65c4207dfd92
pofagyalu <pofagyalu@gmail.com>
2024-03-04 11:18:44 +0100

    added animations and routes to app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+
+import {
+  RouterOutlet,
+  RouterLink,
+  RouterLinkActive,
+  ChildrenOutletContexts,
+} from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { slideInAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+  ],
+  animations: [slideInAnimation],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private contexts: ChildrenOutletContexts) {}
+
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.[
+      'animation'
+    ];
+  }
+}
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ToursComponent } from './tours/tours.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, data: { animation: 'HomePage' } },
+  { path: 'taborok', component: ToursComponent },
+  {
+    path: 'rolunk',
+    component: AboutComponent,
+    data: { animation: 'AboutPage' },
+  },
+  { path: '**', component: NotFoundComponent },
+];
