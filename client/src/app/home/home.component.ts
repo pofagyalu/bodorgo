aa8d3fd1aeb8db057b970a51227a501726b4db4f
pofagyalu <pofagyalu@gmail.com>
2024-03-06 20:37:22 +0100

    added new component to home page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { WeatherModule } from '../weather/weather.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, WeatherModule],
+  imports: [HeroComponent, WeatherModule, NotificationsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
